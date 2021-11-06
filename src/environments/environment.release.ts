2766c625e7d7078446fdb5a9d0c21ad6ee2fe1df
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2021-11-06 15:04:37 +0100

    chore: release 9.0.0

--- src/environments/environment.release.ts
+++ src/environments/environment.release.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   showDevModule: false,
   hmr: false,
-  imgPath: '/angular-modal-gallery-2020-v8.github.io'
+  imgPath: '/angular-modal-gallery-2021-v9.github.io'
 };
